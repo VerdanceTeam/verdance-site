870a2ec0e8ee3989b80d99ee3aaaa6a623ed2847
etanb <etan.berkowitz@gmail.com>
2024-06-12 11:56:35 -0700

    add ashby + responsive header

--- src/_data/jobs.js
+++ src/_data/jobs.js
@@ -3,16 +3,15 @@ const EleventyFetch = require('@11ty/eleventy-fetch');
 // This just sample data to mimic an API response until we add in our Lever jobs
 module.exports = async function () {
     try {
-        let url =
-            'https://gist.githubusercontent.com/etanb/de4963c9de226aa16bd713571dfec6a0/raw/60037a853b5e76054ff17689e7cb2522014b72df/sample_jobs.json';
+        let url = 'https://api.ashbyhq.com/posting-api/job-board/verdance';
 
-        return EleventyFetch(url, {
+        let response = await EleventyFetch(url, {
             duration: '1d',
             type: 'json',
         });
+
+        return response.jobs;
     } catch (e) {
-        return {
-            // my failure fallback data
-        };
+        return [];
     }
 };
