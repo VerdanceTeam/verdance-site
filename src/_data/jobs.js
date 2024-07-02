1fbfa1a727ccb1d471828d6fcb595a08535dd491
etanb <etan.berkowitz@gmail.com>
2024-07-02 11:32:54 -0700

    add logic to stylize pages when no jobs present

--- src/_data/jobs.js
+++ src/_data/jobs.js
@@ -1,17 +1,17 @@
-const EleventyFetch = require('@11ty/eleventy-fetch');
+const EleventyFetch = require("@11ty/eleventy-fetch");
 
 // This just sample data to mimic an API response until we add in our Lever jobs
 module.exports = async function () {
-    try {
-        let url = 'https://api.ashbyhq.com/posting-api/job-board/verdance';
+  try {
+    let url = "https://api.ashbyhq.com/posting-api/job-board/verdance";
 
-        let response = await EleventyFetch(url, {
-            duration: '1d',
-            type: 'json',
-        });
+    let response = await EleventyFetch(url, {
+      duration: "1d",
+      type: "json",
+    });
 
-        return response.jobs;
-    } catch (e) {
-        return [];
-    }
+    return response.jobs;
+  } catch (e) {
+    return [];
+  }
 };
