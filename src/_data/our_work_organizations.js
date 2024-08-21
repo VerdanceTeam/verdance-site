4a0b55ab9567deb59d5ccabe1b0bbe20e554abab
Luke Short <lukeshortemail@gmail.com>
2024-08-21 13:51:39 -0700

    include website screenshots for organizations

--- src/_data/our_work_organizations.js
+++ src/_data/our_work_organizations.js
@@ -17,16 +17,16 @@ module.exports = async function () {
     "description": "The Data at the Point of Care API, currently in pilot mode, lets Medicare beneficiaries ensure their providers have access to their medical information from different doctors, so their medical team can deliver a higher standard of care. We’re helping make the changes to roll out this API to far larger groups, ensuring it’s secure, resilient, and accurate.",
     "logo": "/assets/img/partners/CMS-logo.png",
     "name": "Centers for Medicare & Medicaid Services",
-    "screenshot": "https://image.similarpng.com/very-thumbnail/2020/07/Frowning-Face-emoji-on-transparent-PNG.png",
+    "screenshot": "/assets/img/screenshots/cms-screenshot.png",
     "subheading": "In 2023, health care data still isn’t where it needs to be to be most useful.",
     "type_of_work": "API work - data migration",
   });
   ourWorkOrganizations.push({
     "caption": "This caption describes the image above",
     "description": "We’re helping CDC build a platform called ReportStream to ensure we have better quality disease data when we need it, to support any condition that becomes necessary. The platform is rolled out in 35+ states and counting.",
-    "logo": "https://image.similarpng.com/very-thumbnail/2020/07/Frowning-Face-emoji-on-transparent-PNG.png",
+    "logo": "/assets/img/partners/CDC-logo.png",
     "name": "Centers for Disease Control",
-    "screenshot": "https://image.similarpng.com/very-thumbnail/2020/07/Frowning-Face-emoji-on-transparent-PNG.png",
+    "screenshot": "/assets/img/screenshots/cdc-screenshot.png",
     "subheading": "The COVID years exposed a lack of national technology infrastructure to seamlessly and accurately report disease information.",
     "type_of_work": "API work - data migration",
   });
